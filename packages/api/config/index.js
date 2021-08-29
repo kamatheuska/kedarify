a3700d538cab8d049a3aa64c2b4de6e590f472f1
Nicolas Ramirez <nico9017@gmail.com>
2021-08-29 20:24:23 +0200

    adusting server to connect to mongodb

--- packages/api/config/index.js
+++ packages/api/config/index.js
@@ -2,30 +2,47 @@ const dotenv = require("dotenv");
 const { logDebug } = require("../services/logger");
 const { getEnvVar } = require("../utils/config");
 
-const isTest = process.env.NODE_ENV === "test";
-const isDevelopment = process.env.NODE_ENV === "development";
+const isTest = () => process.env.NODE_ENV === "test";
+const isDevelopment = () => process.env.NODE_ENV === "development";
 let $config;
 
 const getNodeEnvIndependentEnvs = () => ({
   port: getEnvVar("PORT", 5000, Number),
-  nodeEnv: getEnvVar("NODE_ENV"),
   debugMode: getEnvVar("DEBUG_MODE", "false", Boolean),
+  nodeEnv: getEnvVar("NODE_ENV"),
 });
 
+const getMongoDbUri = () => {
+  if (isTest()) {
+    return "mongodb://localhost:27017/kedarify-test";
+  } else if (isDevelopment()) {
+    return getEnvVar("MONGODB_URI", "mongodb://localhost:27017/kedarify");
+  } else {
+    return getEnvVar("MONGODB_URI");
+  }
+};
+
+const getShowroomBaseUrl = () => {
+  return isDevelopment
+    ? "http://localhost:3000"
+    : getEnvVar("SHOWROOM_BASE_URL");
+};
+
 function buildConfigByEnvironment() {
-
-  const showroomBaseUrl = isDevelopment ? getEnvVar('SHOWROOM_BASE_URL') : 'http://localhost:3000';
-
+  const showroomBaseUrl = getShowroomBaseUrl();
+  const mongoDbUri = getMongoDbUri();
   return {
     ...getNodeEnvIndependentEnvs(),
     isTest,
     isDevelopment,
     showroomBaseUrl,
+    mongoDbUri,
   };
 }
 
 function initConfig() {
   dotenv.config();
+  console.log("Printing- - - - process.env.NODE_ENV:", process.env.NODE_ENV);
   $config = buildConfigByEnvironment();
 
   logDebug("initConfig", "INITIAL CONFIG", $config);
